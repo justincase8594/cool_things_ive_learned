a5a4ae52aa0c51b5a22c164df2cb1628c57246cc
Justin R <justincase8594@gmail.com>
2019-11-20 16:49:37 -0600

    Add recaman2.py

---
+++ recaman2.py
@@ -0,0 +1,37 @@
+scale = 5 #this scale isn't turtle, just for us
+stan.speed(900)
+stan.color('red', 'green')
+
+
+
+
+
+
+# move to left for more room
+stan.penup()
+stan.setpos(-390, 0)
+stan.pendown()
+
+current = 0 # Here's how we know where we are
+seen = set() # how we keep track
+
+# step increases by one
+for step_size in range(1, 100):
+
+        backwards = current - step_size
+
+            # go backwords if possible
+                if backwards > 0 and backwards not in seen:
+                            stan.setheading(90) # straight up
+                                    #makes half circle
+                                            stan.circle(scale * step_size/2, 180)
+                                                    current = backwards
+                                                            seen.add(current)
+                                                                # else go forwards
+                                                                    else:
+                                                                                stan.setheading(270) # down
+                                                                                        stan.circle(scale * step_size/2, 180)
+                                                                                                current += step_size
+                                                                                                        seen.add(current)
+
+                                                                                                        turtle.done()
