91a666ab0f8c9ea6677149bbee62b22ea01aee52
Justin R <justincase8594@gmail.com>
2019-12-02 10:36:30 -0600

    list_practice

---
+++ list_practice.py
@@ -0,0 +1,11 @@
+# taking values from a list can be a weird thing to think about.
+# for loops are one of the most common ways to take values out of a list
+# you can also make anothier list with a for loop and appending if the condition is True
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+
+for i in a:
+    if i < 5:
+        b.append(i)
+print(b)
